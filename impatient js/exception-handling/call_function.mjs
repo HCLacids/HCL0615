0841e0845a61529ef3f0f12e1236a44c53d77c8f
盐酸 <55933745+HCLacids@users.noreply.github.com>
2020-07-20 20:43:18 +0800

    Add files via upload

---
+++ impatient js/exception-handling/call_function.mjs
@@ -0,0 +1,12 @@
+export function callFunction(func) {
+    try {
+        const result = func();
+        return {
+            success: result,
+        };
+    } catch (err) {
+        return {
+            failure: err,
+        };
+    }
+}
