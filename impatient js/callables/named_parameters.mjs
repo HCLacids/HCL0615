d24c0e59a7db23511a36d70d33844bcbe357b5ff
盐酸 <55933745+HCLacids@users.noreply.github.com>
2020-07-20 21:36:03 +0800

    callables

---
+++ impatient js/callables/named_parameters.mjs
@@ -0,0 +1,3 @@
+export function handleNamedParameters({ x = 0, y = 0, color = 'black' } = {}) {
+    return [x, y, color];
+}
---
+++ impatient js/callables/positional_parameters.mjs
@@ -0,0 +1,7 @@
+export function twoParametersWithDefaultValues(x = 0, y = 0) {
+    return [x, y]
+}
+
+export function restParameters(x, ...y) {
+    return y;
+}
