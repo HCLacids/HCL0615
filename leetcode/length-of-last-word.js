996862eee28d85d4e3e4d5635598f1038ae6de44
盐酸 <55933745+HCLacids@users.noreply.github.com>
2020-07-25 21:39:14 +0800

    Create length-of-last-word.js

---
+++ leetcode/length-of-last-word.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let end = s.length - 1;
+    while(end >= 0 && s[end] == ' ') end--;
+    if(end < 0) return 0;
+    let start = end;
+    while(start >= 0 && s[start] != ' ') start--;
+    return end - start;
+};
