fdc06242f210dde31295f92be1429f8625a33071
盐酸 <55933745+HCLacids@users.noreply.github.com>
2020-10-13 23:10:44 +0800

    Update 5-4.cpp

--- 数据结构/5-4.cpp
+++ 数据结构/5-4.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 void delete_range(SqList &List, int s, int t){
-    if(s > t || s >= List.length || !List.length) return;
-    while (List.elem[t - 1]){
+   if(s > t || s >= List.length || !List.length) return;
+    while (List.elem[t]){
         List.elem[s++] = List.elem[t++];
         List.length += s-t-1;
     }
@@ -19,9 +19,10 @@ int main(){
     InitList_Sq(list,10,10);
     for (ElemType i = 0; i < 10; i++){
         list.elem[i] = i;
+        list.length++;
     }
     delete_range(list,2,3);
-    for (ElemType i = 0; i < 8; i++){
+    for (ElemType i = 0; i < 9; i++){
         printf("%d",list.elem[i]);
     }
     puts("\n");
