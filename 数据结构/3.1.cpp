2e014283a2f7554e0f22a34c47cf9322aa6f10b2
盐酸 <55933745+HCLacids@users.noreply.github.com>
2020-11-13 16:29:36 +0800

    Create 3.1.cpp

---
+++ 数据结构/3.1.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+#include "DSqString.h"
+using namespace std;
+
+void count_char(DSqString string){
+    int AtoZ[26] = {0};
+    int number[10] = {0};
+    for (int i = 0; i < string.length; i++){
+        if(string.str[i] >= 'A' && string.str[i] <= 'Z'){
+            AtoZ[string.str[i] - 'A']++;
+        }
+        else if(string.str[i] >= '0' && string.str[i] <= '9'){
+            number[string.str[i] - '0']++;
+        }
+    }
+    cout<<"出现的字母有："<<endl;
+    for (int i = 0; i < 26; i++){
+        if(AtoZ[i]) printf("%c的个数为%d\n",i + 65,AtoZ[i]);
+    }
+    cout<<"出现的数字有："<<endl;
+    for (int i = 0; i < 10; i++){
+        if(AtoZ[i]) cout<<i<<"的个数为"<<number[i]<<endl;
+    }
+}
+
+int main(){
+    DSqString str;
+    StrAssign_Sq(str,"1234567890ASDFSAF");
+    count_char(str);
+    return 0;
+}
