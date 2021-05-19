155c6434522d8ac50fdefae9fa8d3e7fb3396147
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-05-19 21:27:47 +0800

    Update replaceSpace.js

--- leetcode/replaceSpace.js
+++ leetcode/replaceSpace.js
@@ -2,9 +2,18 @@
  * @param {string} s
  * @return {string}
  */
+// var replaceSpace = function(s) {
+//     let words = s.split(' ');
+//     let res = '';
+//     words.forEach((a)=>{res += a + "%20"});
+//     return res.replace(/%20$/,"");
+// };
+
 var replaceSpace = function(s) {
-    let words = s.split(' ');
-    let res = '';
-    words.forEach((a)=>{res += a + "%20"});
-    return res.replace(/%20$/,"");
+    let res = "";
+    for (let c of s){
+        if(c !== " ") res += c;
+        else res += "%20";
+    }
+    return res;
 };
