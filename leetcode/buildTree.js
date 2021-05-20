7dce0ce9a158193de8015b5d1f4d87d7d7c533ba
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-05-20 09:08:56 +0800

    Update buildTree.js

--- leetcode/buildTree.js
+++ leetcode/buildTree.js
@@ -10,12 +10,35 @@
  * @param {number[]} inorder
  * @return {TreeNode}
  */
-var buildTree = function(preorder, inorder) {
+// var buildTree = function(preorder, inorder) {
+//     if(!preorder.length) return null;
+//     let head = new TreeNode(preorder[0]);
+
+//     let index = inorder.indexOf(head.val);
+//     head.left = buildTree(preorder.slice(1, index + 1),inorder.slice(0, index));
+//     head.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
+//     return head;
+// };
+
+var buildTree = function(preorder, inorder){
     if(!preorder.length) return null;
     let head = new TreeNode(preorder[0]);
+    let stack = [head];
+    let index = 0;
 
-    let index = inorder.indexOf(head.val);
-    head.left = buildTree(preorder.slice(1, index + 1),inorder.slice(0, index));
-    head.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
+    for(let a = 1; a < preorder.length; a++) {
+        let node = stack[stack.length - 1];
+        if(node.val !== inorder[index]){
+            node.left = new TreeNode(preorder[a]);
+            stack.push(node.left);
+        } else {
+            while(stack.length !== 0 && stack[stack.length - 1].val === inorder[index]){
+                node = stack.pop();
+                index++;
+            }
+            node.right = new TreeNode(preorder[a]);
+            stack.push(node.right);
+        }
+    }
     return head;
-};
+}
