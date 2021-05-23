0cfd8bff83e9ca6b81bb45f10fd5f615643fac85
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-05-23 16:34:12 +0800

    Update movingCount.js

--- leetcode/movingCount.js
+++ leetcode/movingCount.js
@@ -9,13 +9,26 @@ let sum = (num)=>{
     return arr.reduce((a,b)=> {return Number(a) + Number(b)}, 0);
 }
 
-let dfs = (visited, m, n, k, row, col) => {
-    if(row >= m || col >= n || k < sum(row) + sum(col) || visited[row][col]) return 0;
-    visited[row][col] = true;
-    return 1 + dfs(visited, m, n, k, row + 1, col) + dfs(visited, m, n, k, row, col + 1);
-}
+// let dfs = (visited, m, n, k, row, col) => {
+//     if(row >= m || col >= n || k < sum(row) + sum(col) || visited[row][col]) return 0;
+//     visited[row][col] = true;
+//     return 1 + dfs(visited, m, n, k, row + 1, col) + dfs(visited, m, n, k, row, col + 1);
+// }
 
-var movingCount = function(m, n, k ) {
+// var movingCount = function(m, n, k ) {
+//     if(k === 0) return 1;
+//     let visited = new Array();
+//     for(let a  = 0; a < m; a++){
+//         visited[a] = new Array();
+//         for(let b = 0; b < n; b++){
+//             visited[a][b]= false;
+//         }
+//     }
+//     return dfs(visited, m, n, k, 0, 0);
+// };
+
+var movingCount = function(m, n, k) {
+    let res = 0;
     if(k === 0) return 1;
     let visited = new Array();
     for(let a  = 0; a < m; a++){
@@ -24,5 +37,14 @@ var movingCount = function(m, n, k ) {
             visited[a][b]= false;
         }
     }
-    return dfs(visited, m, n, k, 0, 0);
-};
+    let queue = [{row: 0, col: 0}];
+    while(queue.length) {
+        let {row, col} = queue.shift();
+        if(row >= m || col >= n || k < sum(row) + sum(col) || visited[row][col]) continue;
+        visited[row][col] = true;
+        res += 1;
+        queue.push({row: row + 1, col});
+        queue.push({row, col: col + 1});
+    }
+    return res;
+}
