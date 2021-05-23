ae29a97496a3b77390e06bdd672ffff02d18f950
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-05-23 15:38:14 +0800

    Create movingCount.js

---
+++ leetcode/movingCount.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+let sum = (num)=>{
+    let arr = num.toString().split('');
+    return arr.reduce((a,b)=> {return Number(a) + Number(b)}, 0);
+}
+
+let dfs = (visited, m, n, k, row, col) => {
+    if(row >= m || col >= n || k < sum(row) + sum(col) || visited[row][col]) return 0;
+    visited[row][col] = true;
+    return 1 + dfs(visited, m, n, k, row + 1, col) + dfs(visited, m, n, k, row, col + 1);
+}
+
+var movingCount = function(m, n, k ) {
+    if(k === 0) return 1;
+    let visited = new Array();
+    for(let a  = 0; a < m; a++){
+        visited[a] = new Array();
+        for(let b = 0; b < n; b++){
+            visited[a][b]= false;
+        }
+    }
+    return dfs(visited, m, n, k, 0, 0);
+};
