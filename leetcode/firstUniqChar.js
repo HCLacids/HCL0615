1d285b512c08e96fe8062cc7a97637badb6c6a82
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-08-11 20:10:58 +0800

    Create firstUniqChar.js

---
+++ leetcode/firstUniqChar.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var firstUniqChar = function(str) {
+    if (!str) return ' ';
+    const m = new Map();
+    for (const s of str) {
+        m.set(s, (m.get(s) || 0) + 1);
+    }
+    for (const s of m) {
+        if (s[1] === 1) return s[0];
+    }
+    return ' ';
+};
