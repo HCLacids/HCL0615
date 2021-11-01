57280212d028bd174a7b265563e895bab2bf633b
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-11-01 22:25:09 +0800

    Delete distributeCandies.ts

--- distributeCandies.ts
+++
@@ -1,8 +0,0 @@
-function distributeCandies(candyType: number[]): number {
-    const n = candyType.length;
-    const eatCount = n / 2;
-    let mySet = new Set(candyType);
-    const typeCount = mySet.size;
-
-    return typeCount > eatCount ? eatCount : typeCount;
-};
