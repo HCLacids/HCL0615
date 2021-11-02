26c0353560c75d2efb3e492ef38704fb5b3bf016
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-11-02 16:48:15 +0800

    Create deleteNode.ts

---
+++ leetcode/deleteNode.ts
@@ -0,0 +1,17 @@
+// Definition for singly-linked list.
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+        }
+    }
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+ function deleteNode(root: ListNode | null): void {
+    root.val = root.next.val;
+    root.next = root.next.next;
+};
