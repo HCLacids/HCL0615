72d3e624b031f6c733f583be6e35d7bedca8be82
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-11-05 21:35:10 +0800

    Delete longestSubsequence.ts

--- css_in_depth/longestSubsequence.ts
+++
@@ -1,9 +0,0 @@
-function longestSubsequence(arr: number[], difference: number): number {
-    let ans = 0;
-    let map = new Map();
-    for(let a of arr){
-        map.set(a, (map.get(a - difference) || 0) + 1);
-        ans = Math.max(ans, map.get(a));
-    }
-    return ans;
-};
