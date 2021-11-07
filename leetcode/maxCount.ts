41833c169d33c5063965f568a0fb8e4f570015a6
盐酸 <55933745+HCLacids@users.noreply.github.com>
2021-11-07 11:29:27 +0800

    Create maxCount.ts

---
+++ leetcode/maxCount.ts
@@ -0,0 +1,8 @@
+ function maxCount(m: number, n: number, ops: number[][]): number {
+   let mina = m, minb = n;
+      for (const op of ops) {
+          mina = Math.min(mina, op[0]);
+          minb = Math.min(minb, op[1]);
+      }
+      return mina * minb;
+}
